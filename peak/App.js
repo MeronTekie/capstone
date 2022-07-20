ce1ef679b8e6b9586cfdf029757d979789cac351
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-07-19 22:26:47 -0700

    Install expo sensors to the project"

--- peak/App.js
+++ peak/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground, Pedo } from "react-native";
+// import { Pedometer } from "expo-sensors";
+// import React ,{useState}from "react";
+
+// const [pedometer,setPedometer] = useState();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.navbar}>
+				<Text> THERE</Text>
+			</View>
+			<ImageBackground
+				style={{ flex: 1 }}
+				resizeMode="stretch"
+				source={require("./assets/pedometer-cover.jpeg")}
+			>
+				<View style={styles.navbar}>
+					<Text> THERE</Text>
+				</View>
+			</ImageBackground>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		// backgroundColor: '#fff',
+		// alignItems: 'center',
+		// justifyContent: 'center',
+	},
+	navbar: {
+		marginTop: 100,
+    justifyContent:'center',
+	},
 });
