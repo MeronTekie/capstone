a92178d0b2671d3716d91b11d9a0477c5c0e6343
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-07-20 05:09:52 -0700

    Set the protoype for the Healty Info data

--- peak/App.js
+++ peak/App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, ImageBackground, Button } from "react-native";
-// import { Pedometer } from "expo-sensors";
 import React ,{useState} from "react";
 import PedometerTracker from "./components/Pedometer";
 
