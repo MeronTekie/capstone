621e790aedc75e31017d1b40d29d4fa7a57c8157
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-07-20 19:02:53 -0700

    Add the functionality to count steps

--- peak/components/Pedometer.js
+++ peak/components/Pedometer.js
@@ -1,37 +1,41 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, ImageBackground, Button } from "react-native";
 import { Pedometer } from "expo-sensors";
 
-
-
 function PedometerTracker() {
-  const [availability,setAvailability]=useState('')
-  // const [pedometer,setPedometer] = useState();
-  Pedometer.isAvailableAsync().then(
-    (result)=>{
-      setAvailability(String(result))
-    }, 
-    (error)=>{
-      setAvailability(error);
-    }
-  )
-  return (
-      <View style={styles.container}>
-        <View style={styles.navbar}>
-          <Text> THERE</Text>
-        </View>
-        <ImageBackground
-          style={{ flex: 1 }}
-          resizeMode="stretch"
-          source={require("../assets/pedometer-cover.jpeg")}
-        >
-          <View style={styles.navbar}>
-            <Text style={styles.navbar}> {availability}</Text>
-          </View>
-        </ImageBackground>
-      </View>
-    );
+	const [availability, setAvailability] = useState("");
+	const [stepsNumber, setStepsNumber] = useState(0);
+
+	Pedometer.isAvailableAsync().then(
+		(result) => {
+			setAvailability(String(result));
+		},
+		(error) => {
+			setAvailability(error);
+		}
+	);
+	subscribe = () => {
+		const subscription = Pedometer.watchStepCount((result) => {
+			setStepsNumber(result.steps);
+		});
+	};
+	return (
+		<View style={styles.container}>
+			<ImageBackground
+				style={{ flex: 1 }}
+				resizeMode="stretch"
+				source={require("../assets/pedometer-cover.jpeg")}
+			>
+				<View style={styles.navbar}>
+					<Text style={styles.status}> Pedometer:{availability}</Text>
+				</View>
+				<View style={styles.navbar}>
+					<Text> Number of Steps: {stepsNumber}</Text>
+				</View>
+			</ImageBackground>
+		</View>
+	);
 }
 const styles = StyleSheet.create({
 	container: {
@@ -40,13 +44,17 @@ const styles = StyleSheet.create({
 		// alignItems: 'center',
 		// justifyContent: 'center',
 	},
-	navbar: {
-		marginTop: 100,
-    justifyContent:'center',
-    color:'white',
-    fontSize:45,
-    alignItems:'center'
+	status: {
+		marginTop: 500,
+		justifyContent: "center",
+		color: "white",
+		fontSize: 30,
+		alignItems: "center",
+		backgroundColor: "rgba(15,189,182,0.4)",
+		width: 400,
+		textAlign: "center",
+		fontWeight: "bold",
 	},
 });
 
-export default PedometerTracker
+export default PedometerTracker;
