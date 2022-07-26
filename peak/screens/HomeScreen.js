da23f004560312aa7a843fe6b50f801f4f3086c2
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-07-26 14:01:17 -0700

    Create a progress chart for the project

--- peak/screens/HomeScreen.js
+++ peak/screens/HomeScreen.js
@@ -5,22 +5,22 @@ import Swiper from "react-native-swiper";
 const styles = StyleSheet.create({
   wrapper: {},
   slide1: {
-    flex: 1,
+    flex: 0.8,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#9DD6EB'
+    backgroundColor: '#9FD6FB'
   },
   slide2: {
-    flex: 1,
+    flex: 0.8,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#97CAE5'
   },
   slide3: {
-    flex: 1,
+    flex: 0.8,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#92BBD9'
+    backgroundColor: '#47BBD9'
   },
   text: {
     color: '#fff',
@@ -33,10 +33,26 @@ export default function Home({ navigation }) {
 	return (
       <Swiper style={styles.wrapper} showsButtons={true}>
         <View style={styles.slide1}>
+          <Image style={{height:280,width:280,borderRadius:140}} source={require('../assets/pedometer-cover.jpeg')} />
           <Text style={styles.text}>Hello Swiper</Text>
         </View>
         <View style={styles.slide2}>
           <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
         </View>
         <View style={styles.slide3}>
           <Text style={styles.text}>And simple</Text>
--- peak/screens/Progress.js
+++ peak/screens/Progress.js
@@ -1,21 +1,58 @@
-import * as React from 'react';
-import { View,Text,StyleSheet } from 'react-native';
-export default function Progress({navigation}){
-  return(
-    <View style={styles.homeContainer}>
-      <Text style={styles.homeBody} onPress={()=>alert('Hi There!')}> Home</Text>
-    </View>
-  )
-}
-const styles = StyleSheet.create({
-  homeContainer:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-  },
-  homeBody:{
-    fontSize:25,
-    fontWeight:'bold',
-  }
 
-})
+import * as React from "react";
+import { Text, View, StyleSheet, Dimensions, ScrollView } from "react-native";
+import {
+	LineChart,
+	BarChart,
+	PieChart,
+	ProgressChart,
+	ContributionGraph,
+	StackedBarChart,
+} from "react-native-chart-kit";
+export default function Progress({ navigation }) {
+	return (
+		<ScrollView>
+			<View style={styles.container}>
+				<View>
+					{/*It is an Example of Progress Chart*/}
+					<Text
+						style={{
+							textAlign: "center",
+							fontSize: 48,
+							padding: 36,
+							marginTop: 86,
+						}}
+					>
+						Progress 
+					</Text>
+					<ProgressChart
+						data={[0.2, 0.7, 0.5]}
+						width={Dimensions.get("window").width - 16}
+						height={220}
+						chartConfig={{
+							backgroundColor: "white",
+							backgroundGradientFrom: "white",
+							backgroundGradientTo: "white",
+							decimalPlaces: 2,
+							color: (opacity = 0.4) => `rgba(25, 18, 189, ${opacity})`,
+							style: {
+								borderRadius: 26,
+							},
+						}}
+						style={{ marginVertical: 8, borderRadius: 16 }}
+					/>
+				</View>
+			</View>
+		</ScrollView>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		padding: 8,
+		paddingTop: 30,
+		backgroundColor: "white",
+	},
+});
